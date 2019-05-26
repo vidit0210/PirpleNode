1ca4d5ca2b60efa23fe1bc080b1fbd22959d167d
Vidit Shah <vidit0210@gmail.com>
2019-05-26 10:11:52 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -2,12 +2,11 @@
 //Dependencies
 
 const http = require('http');
-let url = require('url');
+const url = require('url');
+const stringDecoder =require('string_decoder').StringDecoder;
 
 let server= http.createServer((req,res)=>{
-     //QueryString
-
-    let queryString = url.parse(req.url).query;
+    
     
     // let parsedUrl = url.parse(req.url,true);
     // let path = parsedUrl.pathname;
@@ -23,10 +22,28 @@ let server= http.createServer((req,res)=>{
     //Type of Method which being Utlilzed;
     let method = req.method.toLowerCase();
 
-    res.end("Hello world\n");
-    console.log("Requested path is "+parsedUrl +'Rquested Method is : ' + method+'query : '+ queryString );
-    
+    //Get Payload if there any
+    let decoder = new stringDecoder('utf-8');
+    let  buffer ='';
+    req.on('data',(data)=>{
+        buffer+= decoder.write(data)
+    })
+    req.on('end',()=>{
+        buffer+=decoder.end()
+        res.end("Hello world\n");
+        console.log("Headers are:",headers);
+        console.log('playlods are:',buffer)
+        
+    })
+
 
 })
 
 server.listen(3000,()=>{console.log('Listening to Port 3000')});
+
+let handlers ={}
+handlers.sample = function(data,callback){
+    // CallBack should give HTTP status code and payload
+    callback(406,{'name':'handler Sample'})
+    
+}
